799a4bb84eea3d8c46786dd1f80d36225303d8ef
mark436 <marcosochoarobles@gmail.com>
2025-10-01 16:21:01 -0700

    upload practica5

---
+++ practica5/HilosProy/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace HilosProy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 87);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(169, 120);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = " ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lavender;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(14, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hilo 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lavender;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(14, 47);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Hilo 2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lavender;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(14, 81);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 27);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hilo 3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Lavender;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(14, 114);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 27);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Hilo 4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(321, 196);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ practica5/HilosProy/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Reflection.Emit;
+
+namespace HilosProy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Thread hilo1, hilo2, hilo3, hilo4;
+        bool hilo1Ini = false;
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (hilo2 == null || !hilo2.IsAlive) 
+            {
+                hilo2 = new Thread(() =>
+                {
+                    Thread.Sleep(3000); 
+                    Invoke((MethodInvoker)(() => label2.Text = "Hilo 2"));
+                    if (hilo1 != null && hilo1.ThreadState == ThreadState.Stopped)
+                    {
+                        hilo1 = null;
+                        Invoke((MethodInvoker)(() => label1.Text = ""));
+                    }
+                    else if (hilo1 != null && hilo1.ThreadState == ThreadState.Suspended)
+                    {
+                        hilo1.Resume(); 
+                        hilo1 = null;   
+                        Invoke((MethodInvoker)(() => label1.Text = ""));
+                    }
+                });
+
+                hilo2.Start();
+            }
+            else
+            {
+                
+                MessageBox.Show("Hilo 2 ya está en ejecución", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+
+            if (hilo3 == null)
+            {
+                hilo3 = new Thread(() =>
+                {
+                    Thread.CurrentThread.Suspend();
+
+                    label3.Text = "Hilo 3";
+
+                    Thread.Sleep(5000);
+                    Application.Exit();
+                });
+
+                hilo3.Start();
+            }
+            else
+            {
+
+                MessageBox.Show("Hilo 3 ya está en ejecución", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {if(hilo3 == null)
+            {
+                MessageBox.Show("Inicie el Hilo 3", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (hilo4 == null )
+            {
+                hilo4 = new Thread(() =>
+                {
+                    Thread.Sleep(5000);
+                    label4.Text = "Hilo 4";
+
+                    if (hilo3 != null && hilo3.ThreadState == ThreadState.Suspended)
+                    {
+                        hilo3.Resume();
+                    }
+                });
+
+                hilo4.Start();
+            }
+            else
+            {
+
+                MessageBox.Show("Hilo 4 ya está en ejecución", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (hilo1 == null)
+            {
+                hilo1 = new Thread(() =>
+                {
+                    
+                        Int64 x, y = 0;
+                        for (x = 1; x <= 300000; x++)
+                        {
+                            y += x;
+
+                            if (x == 1)
+                            {
+                                Thread.CurrentThread.Suspend(); // Suspender después de la primera iteración
+                            }
+                        }
+                        Invoke((MethodInvoker)(() => label1.Text = y.ToString()));
+                    
+                    
+                });
+
+                hilo1.Start();
+            }
+            else if (hilo1.ThreadState == ThreadState.Suspended)
+            {
+                hilo1.Resume(); // Reanuda si está suspendido
+            }
+            else
+            {
+
+                MessageBox.Show("Hilo 1 ya está en ejecución", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if ((hilo1 != null && hilo1.IsAlive && hilo1.ThreadState == ThreadState.Suspended) ||
+                (hilo2 != null && hilo2.IsAlive && hilo2.ThreadState == ThreadState.Suspended) ||
+                (hilo3 != null && hilo3.IsAlive && hilo3.ThreadState == ThreadState.Suspended) ||
+                (hilo4 != null && hilo4.IsAlive && hilo4.ThreadState == ThreadState.Suspended))
+            {
+                MessageBox.Show("Hay un hilo suspendido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                e.Cancel = true; 
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
